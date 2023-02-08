338074266644cbae0d310376044187e06dc927c6
Edugoda314 <camiloedg23@gmail.com>
2023-02-08 07:16:19 -0800

    Automated firebase python json update

--- python_firebase_data_update.py
+++ python_firebase_data_update.py
@@ -1,42 +1,63 @@
-from tkinter import FALSE
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import time
-
-# Use a service account to access Firebase
-# cred = credentials.Certificate('C:\Users\Edugoda\coding\Python\UparkingPython\uparkingpreview-firebase-adminsdk-uxlsw-33fb14f2db.json')
-cred = credentials.Certificate(r'C:\Users\Edugoda\coding\Python\UparkingPython\uparkingpreview-firebase-adminsdk-uxlsw-33fb14f2db.json')
-
-firebase_admin.initialize_app(cred)
-
-# Connect to Firestore
-db = firestore.client()
-
-# Reference to the collection
-collection_ref = db.collection(u'imgMapParking')
-n = 0
-while True:
-    # Use the where() method to filter the documents you want to update
-    query = collection_ref.where("name", "==", "img_1")
-
-    # Update the filtered documents
-    docs = query.get()
-    n = n + 1
+import json
+json_data = {}
+def update_firestore_collection(docs, status_value):
     for doc in docs:
             doc_ref = doc.reference
             doc_ref.update({
-                "status": n ,
-                # "field_2_to_update": new_value_2,
-                # ...
+                "status": status_value ,
             })
+        # Wait for the specified time before updating again
+        # time.sleep(sleep_time)
 
-    # Wait for one minute before updating again
-    time.sleep(10)
-#C:\Users\Edugoda\AppData\Local\Programs\Python\Python311
+def import_json(file_path):
+    # Load the JSON file
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+    return data
 
+def get_values(json_str, key):
+    img_data_array=[]
+    # Load the JSON data into a Python dictionary
+    json_data = json_str["data"]
+    # Check if the key is in the JSON data
+    for data_img in json_data:
+        if key in data_img:
+            # Get the values for the key
+            values = data_img[key]
+            # Return the values as a list
+            img_data_array.append(values)
+        # Key is not in the JSON data
+        else:
+            return "Error empty json"
+    return img_data_array
 
-# Reference the collection you want to update
-collection_ref = db.collection("collection_name")
+def array_distribution2update(collection_ref,img_data):
+
+    for img in img_data:
+        # Use the where() method to filter the documents you want to update
+        query = collection_ref.where("name", "==", img[1])
+        # Update the filtered documents
+        docs = query.get()
+        update_firestore_collection(docs,img)
+    
+
+if __name__ == '__main__':
+     # Use a service account to access Firebase
+    cred = credentials.Certificate(r'C:\Users\Edugoda\coding\Python\UparkingPython\uparkingpreview-firebase-adminsdk-uxlsw-33fb14f2db.json')
+    firebase_admin.initialize_app(cred)
+    # Connect to Firestore
+    db = firestore.client()
+    # Reference to the collection
+    collection_ref = db.collection("imgMapParking")
+    while True:
+        json_data = import_json(r'C:\Users\Edugoda\coding\Python\UparkingPython\python_json_data.json')
+        img_data_array = get_values(json_data,"status")
+        array_distribution2update(collection_ref,img_data_array)
+        time.sleep(10)
+
 
 
