e331bf13a16a837c742f5f5ac18f508228716254
Edugoda314 <camiloedg23@gmail.com>
2023-02-10 21:21:19 -0800

    Some litle changes and a subprocess implementation

---
+++ python_subprocess_init.py
@@ -0,0 +1,13 @@
+import subprocess
+# Start the first program
+process1 = subprocess.Popen(['python', 'python_json_update.py'])
+
+# # Start the second program
+process2 = subprocess.Popen(['python', 'python_firebase_data_update.py'])
+
+# # Wait for both programs to finish
+process1.wait()
+process2.wait()
+
+
+
